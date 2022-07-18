6ab2906d87ebb7fb65bd5d277197e8a212fa5b66
Satoshi Shirato <satoshi.shirato@gmail.com>
2022-07-18 11:19:37 +0900

    fix zkhk key override - change behaviour of zkhk_key_override - add JP key macro translation comment - delete copyright

--- keyboards/mint60/keymaps/pseudo_jis_toggle/keymap.c
+++ keyboards/mint60/keymaps/pseudo_jis_toggle/keymap.c
@@ -1,18 +1,3 @@
-/* Copyright 2018 Eucalyn
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 #include "print.h"
 #include "keymap_japanese.h"
@@ -21,36 +6,46 @@
 #define OVR_OFF KEY_OVERRIDE_OFF
 #define OVR_TOG KEY_OVERRIDE_TOGGLE
 
-// 1. shift + 通常キー → 通常キー
-const key_override_t at_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, JP_AT);
-const key_override_t circ_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_6, JP_CIRC);
-const key_override_t coln_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_SCLN, JP_COLN);
+// 1. shift + key → unmodified key
+const key_override_t at_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, JP_AT);        //JP_AT   :KC_LBRC
+const key_override_t circ_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_6, JP_CIRC);    //JP_CIRC :KC_EQL
+const key_override_t coln_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_SCLN, JP_COLN); //JP_COLN :KC_QUOT
 
-// 2. shift + 通常キー → shift + 通常キー
-const key_override_t ampr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_7, JP_AMPR);
-const key_override_t astr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_8, JP_ASTR);
-const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_9, JP_LPRN);
-const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_0, JP_RPRN);
-const key_override_t unds_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_MINS, JP_UNDS);
-const key_override_t plus_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_EQL, JP_PLUS);
-const key_override_t lcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, JP_LCBR);
-const key_override_t rcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RBRC, JP_RCBR);
-const key_override_t pipe_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSLS, JP_PIPE);
-const key_override_t dquo_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, JP_DQUO);
-const key_override_t tild_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_GRV, JP_TILD);
+// 2. shift + key → shift + key
+const key_override_t ampr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_7, JP_AMPR);    //JP_AMPR :S(KC_6)
+const key_override_t astr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_8, JP_ASTR);    //JP_ASTR :S(KC_QUOT)
+const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_9, JP_LPRN);    //JP_LPRN :S(KC_8)
+const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_0, JP_RPRN);    //JP_RPRN :S(KC_9)
+const key_override_t unds_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_MINS, JP_UNDS); //JP_UNDS :S(KC_INT1)
+const key_override_t plus_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_EQL, JP_PLUS);  //JP_PLUS :S(KC_SCLN)
+const key_override_t lcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LBRC, JP_LCBR); //JP_LCBR :S(KC_RBRC)
+const key_override_t rcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RBRC, JP_RCBR); //JP_RCBR :S(KC_NUHS)
+const key_override_t pipe_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSLS, JP_PIPE); //JP_PIPE :S(KC_INT3)
+const key_override_t dquo_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, JP_DQUO); //JP_DQUO :S(KC_2)
+const key_override_t tild_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_GRV, JP_TILD);  //JP_TILD :S(KC_EQL)
 
-// 3. 通常キー → 通常キー
-const key_override_t lbrc_key_override = ko_make_with_layers_and_negmods(0, KC_LBRC, JP_LBRC, ~0, (uint8_t)MOD_MASK_SHIFT);
-const key_override_t rbrc_key_override = ko_make_with_layers_and_negmods(0, KC_RBRC, JP_RBRC, ~0, (uint8_t)MOD_MASK_SHIFT);
-const key_override_t bsls_key_override = ko_make_with_layers_and_negmods(0, KC_BSLS, JP_BSLS, ~0, (uint8_t)MOD_MASK_SHIFT);
+// 3. unmodified key → unmodified key
+const key_override_t lbrc_key_override = ko_make_with_layers_and_negmods(0, KC_LBRC, JP_LBRC, ~0, (uint8_t)MOD_MASK_SHIFT); //JP_LBRC :KC_RBRC
+const key_override_t rbrc_key_override = ko_make_with_layers_and_negmods(0, KC_RBRC, JP_RBRC, ~0, (uint8_t)MOD_MASK_SHIFT); //JP_RBRC :KC_NUHS
+const key_override_t bsls_key_override = ko_make_with_layers_and_negmods(0, KC_BSLS, JP_BSLS, ~0, (uint8_t)MOD_MASK_SHIFT); //JP_BSLS :KC_INT1
 
-// 4, 通常キー → shift + 通常キー
-const key_override_t eql_key_override = ko_make_with_layers_and_negmods(0, KC_EQL, JP_EQL, ~0, (uint8_t)MOD_MASK_SHIFT);
-const key_override_t quot_key_override = ko_make_with_layers_and_negmods(0, KC_QUOT, JP_QUOT, ~0, (uint8_t)MOD_MASK_SHIFT);
-const key_override_t grv_key_override = ko_make_with_layers_and_negmods(0, KC_GRV, JP_GRV, ~0, (uint8_t)MOD_MASK_SA);
+// 4, unmodified key → shift + key
+const key_override_t eql_key_override = ko_make_with_layers_and_negmods(0, KC_EQL, JP_EQL, ~0, (uint8_t)MOD_MASK_SHIFT);    //JP_EQL  :S(KC_MINS)
+const key_override_t quot_key_override = ko_make_with_layers_and_negmods(0, KC_QUOT, JP_QUOT, ~0, (uint8_t)MOD_MASK_SHIFT); //JP_QUOT :S(KC_7)
+const key_override_t grv_key_override = ko_make_with_layers_and_negmods(0, KC_GRV, JP_GRV, ~0, (uint8_t)MOD_MASK_SA);       //JP_GRV  :S(KC_LBRC)
 
-// 5. alt + 通常キー → 通常キー
-const key_override_t zkhk_key_override = ko_make_basic(MOD_MASK_ALT, KC_GRV, JP_ZKHK);
+// 5. alt + key → unmodified key
+// const key_override_t zkhk_key_override = ko_make_basic(MOD_MASK_ALT, KC_GRV, JP_ZKHK); //JP_ZKHK :KC_GRV
+const key_override_t zkhk_key_override = {.trigger_mods          = MOD_MASK_ALT,
+                                     .layers                 = ~0,
+                                     .suppressed_mods        = 0,
+                                     .options                = ko_options_default,
+                                     .negative_mod_mask      = 0,
+                                     .custom_action          = NULL,
+                                     .context                = NULL,
+                                     .trigger                = KC_GRV,
+                                     .replacement            = JP_ZKHK,   //JP_ZKHK :KC_GRV
+                                     .enabled                = NULL};
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &at_key_override,
